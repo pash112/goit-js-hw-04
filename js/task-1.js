c56dbb58e1fc684380f2d60c7b553fadd1d1df17
Pavlo Markovsky <pash199060@gmail.com>
2025-02-11 13:51:06 +0200

    new

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function isEnoughCapacity(products, containerSize) {
+    const totalProducts = Object.values(products).reduce((sum, amount) => sum + amount, 0);
+
+    return totalProducts <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
